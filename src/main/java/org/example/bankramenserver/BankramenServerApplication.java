e6e5d49dd6a7e90c9ede88bcfa386fc74afd2bf7
sunnyday0206 <sunme8412@gmail.com>
2026-04-01 10:53:16 +0900

    init(#1): 프로젝트 세팅

---
+++ src/main/java/org/example/bankramenserver/BankramenServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.bankramenserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankramenServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankramenServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bankramenserver/BankramenServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bankramenserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankramenServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
