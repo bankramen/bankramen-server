7312041f90c6d30bf18db7c45bc3cb698fc4137a
sunnyday0206 <sunme8412@gmail.com>
2026-04-10 14:14:50 +0900

    feat(#1): SecurityConfig 수정

--- src/main/java/org/example/bankramenserver/global/config/SecurityConfig.java
+++ src/main/java/org/example/bankramenserver/global/config/SecurityConfig.java
@@ -1,5 +1,8 @@
-package org.example.bankramenserver.config;
+package org.example.bankramenserver.global.config;
 
+import lombok.RequiredArgsConstructor;
+import org.example.bankramenserver.global.error.GlobalExceptionFilter;
+import org.example.bankramenserver.global.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,10 +10,15 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final GlobalExceptionFilter globalExceptionFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -23,8 +31,12 @@ public class SecurityConfig {
                         .requestMatchers("/auth/kakao/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(AbstractHttpConfigurer::disable)   // 기본 로그인 페이지 비활성화
-                .httpBasic(AbstractHttpConfigurer::disable);
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(globalExceptionFilter,
+                        JwtAuthenticationFilter.class);
 
         return http.build();
     }
