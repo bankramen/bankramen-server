b27e379fbc0cc15fb7775a5eb06eb2e0dd1630b4
sunnyday0206 <sunme8412@gmail.com>
2026-04-10 12:06:16 +0900

    chore(#1): User 파일 구조 변경

--- src/main/java/org/example/bankramenserver/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/bankramenserver/domain/user/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package org.example.bankramenserver.domain.user.repository;
 
-import org.example.bankramenserver.domain.user.entity.User;
+import org.example.bankramenserver.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
