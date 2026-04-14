cb2a4694715a4eabfaf8f5889846dae6f8ddc0c1
sunnyday0206 <sunme8412@gmail.com>
2026-04-14 21:34:54 +0900

    feat(#2): 수정

--- src/main/java/org/example/bankramenserver/domain/user/exception/UserInfoNotFound.java
+++ src/main/java/org/example/bankramenserver/domain/user/exception/UserInfoNotFound.java
@@ -1,12 +1,11 @@
 package org.example.bankramenserver.domain.user.exception;
 
-import org.example.bankramenserver.domain.auth.exception.MissingTokenException;
 import org.example.bankramenserver.global.error.exception.ErrorCode;
 import org.example.bankramenserver.global.error.exception.GlobalException;
 
 public class UserInfoNotFound extends GlobalException {
 
-    public static final GlobalException EXCEPTION = new MissingTokenException();
+    public static final GlobalException EXCEPTION = new UserInfoNotFound();
 
     public UserInfoNotFound() { super(ErrorCode.USER_INFO_NOT_FOUND); }
 }
