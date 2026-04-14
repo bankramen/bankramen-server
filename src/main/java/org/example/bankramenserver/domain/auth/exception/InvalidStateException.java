673df54e09d1579f57e76c268be57a8355824d06
sunnyday0206 <sunme8412@gmail.com>
2026-04-14 20:14:19 +0900

    feat(#2): InvalidStateException 수정

--- src/main/java/org/example/bankramenserver/domain/auth/exception/InvalidStateException.java
+++ src/main/java/org/example/bankramenserver/domain/auth/exception/InvalidStateException.java
@@ -5,7 +5,7 @@ import org.example.bankramenserver.global.error.exception.GlobalException;
 
 public class InvalidStateException extends GlobalException {
 
-    public static final GlobalException EXCEPTION = new InvalidTokenException();
+    public static final GlobalException EXCEPTION = new InvalidStateException();
 
     public InvalidStateException() { super(ErrorCode.INVALID_STATE); }
 }
