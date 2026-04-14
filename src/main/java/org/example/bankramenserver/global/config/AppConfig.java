8b4dbb945895eb50f3d303fecb00f157d42a2a54
sunnyday0206 <sunme8412@gmail.com>
2026-04-14 20:15:04 +0900

    feat(#2): AppConfig 수정

--- src/main/java/org/example/bankramenserver/global/config/AppConfig.java
+++ src/main/java/org/example/bankramenserver/global/config/AppConfig.java
@@ -1,15 +1,21 @@
 package org.example.bankramenserver.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class AppConfig {
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setConnectTimeout(Duration.ofSeconds(3))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .build();
     }
 
     @Bean
