76ac5d642703b954199c6dd72b8f2859d2887b50
coehgns <modooboiroo@gmail.com>
2026-05-20 15:29:46 +0900

    fix:  ObjectMapper JavaTimeModule 설정 추가

--- src/main/java/org/example/bankramenserver/global/config/AppConfig.java
+++ src/main/java/org/example/bankramenserver/global/config/AppConfig.java
@@ -1,6 +1,8 @@
 package org.example.bankramenserver.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,7 +23,9 @@ public class AppConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        return new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 
     @Bean
